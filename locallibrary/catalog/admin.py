e9f87a4db3a96530153a9cf8ac6306b01ce5b57f
IvanDenisenko-j <wanius89039158476@gmail.com>
2025-09-13 01:43:46 +0700

    crate admin panel

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre', 'language')
+    inlines = [BooksInstanceInline]
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'book', 'status', 'due_back', 'borrower')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        })
+    )
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre)
+admin.site.register(BookInstance, BookInstanceAdmin)
+admin.site.register(Language)
